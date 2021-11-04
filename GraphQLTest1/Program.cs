86d2c9a4d5d5ce73d3e9efef58519f9ab6a00cc3
Renato Costa <renato.rezende.costa@gmail.com>
2021-11-04 12:46:20 +0100

    really dumb graphql sample

---
+++ GraphQLTest1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using GraphQL;
+using GraphQL.Client.Http;
+using GraphQL.Client.Serializer.Newtonsoft;
+
+namespace GraphQLTest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var graphQlClient = new GraphQLHttpClient("http://localhost:8080/graphql", new NewtonsoftJsonSerializer());
+            
+            var artistsRequest = new GraphQLRequest {
+                Query = @"
+                        {
+                            artists {
+                                id
+                                name
+
+                            }
+                        }"
+            };
+
+            var response = Task.Run(async () => await graphQlClient.SendQueryAsync<dynamic>(artistsRequest)).Result;
+            
+            Console.WriteLine("Printing response: ");
+            Console.WriteLine(response.Data);
+            Console.WriteLine("Done");
+        }
+    }
+
+    public class Artist
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
